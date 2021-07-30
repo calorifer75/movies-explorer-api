7681cdf79445a76c42121d018add1ce8b73a439c
Viacheslav Voytenko <calorifer75@yandex.ru>
2021-07-30 15:49:09 +0300

    fix: API не позволяет пользователя без имени

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.patch(
     body: Joi.object()
       .keys({
         email: Joi.string().required().email(),
-        name: Joi.string(),
+        name: Joi.string().required().min(2).max(30),
       }),
   }),
   setUserInfo,
