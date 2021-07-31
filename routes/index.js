19f2e15a59b0ae6468be3b9d5af394ca3b551bf7
Viacheslav Voytenko <calorifer75@yandex.ru>
2021-07-31 11:19:40 +0300

    сообщения об ошибках вынесены в отдельный файл

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const NotFoundError = require('../errors/not-found-err');
+const constants = require('../config/constants');
 
 // создание и логин пользователя
 router.use('/', require('./login'));
@@ -12,6 +13,6 @@ router.use('/users', require('./users'));
 router.use('/movies', require('./movies'));
 
 // обработка ошибки 404
-router.use((req, res, next) => next(new NotFoundError('Ошибка 404. Страница не найдена')));
+router.use((req, res, next) => next(new NotFoundError(constants.pageNotFoundErrorMsg)));
 
 module.exports = router;
